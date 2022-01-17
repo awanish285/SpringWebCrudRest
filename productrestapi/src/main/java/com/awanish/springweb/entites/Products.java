14a330fdafa7940c0b760eb56b904899b6fa0741
awanish285 <45520212+awanish285@users.noreply.github.com>
2022-01-17 19:34:54 +0530

    Adding some comment..

--- productrestapi/src/main/java/com/awanish/springweb/entites/Products.java
+++ productrestapi/src/main/java/com/awanish/springweb/entites/Products.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+//this is our model class
 @Entity
 public class Products {
 	@Id
